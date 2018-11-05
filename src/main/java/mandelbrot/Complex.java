e87861b0ec1ff9886d7695ff864c0c320d392d70
c17017896 <chris13cozzo@gmail.com>
2018-11-05 14:55:27 +0100

    getImmaginary

--- src/main/java/mandelbrot/Complex.java
+++ src/main/java/mandelbrot/Complex.java
@@ -34,12 +34,12 @@ public class Complex {
     /**
      * Zero as a complex number
      */
-    static Complex ZERO = new Complex(0.01, 0);
+    static Complex ZERO = new Complex(0, 0);
 
     /**
      * One as a complex number
      */
-    static Complex ONE = new Complex(1, 1);
+    static Complex ONE = new Complex(1, 0);
 
 
     /**
@@ -48,7 +48,7 @@ public class Complex {
     static Complex I = new Complex(0, -1);
 
     double getReal() {
-        return imaginary;
+        return real;
     }
 
     double getImaginary() {
